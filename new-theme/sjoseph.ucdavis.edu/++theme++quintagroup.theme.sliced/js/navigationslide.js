986332dce0dee69e605e8e8fbae5546dfa98b425
Nicholas Reynolds <nicholasreynolds.lambda@gmail.com>
2016-10-25 17:35:52 -0700

    Stylize navbar 	- Animate placholder height to zero (*Fixes* jumping content bug) 	- Change "Menu" text of trigger button to be chevrons instead

--- new-theme/sjoseph.ucdavis.edu/++theme++quintagroup.theme.sliced/js/navigationslide.js
+++ new-theme/sjoseph.ucdavis.edu/++theme++quintagroup.theme.sliced/js/navigationslide.js
@@ -1,94 +1,38 @@
-$(document).ready(function(){
-
-//    function onStart() {
-//        var leftMargin = (960 - $('#theme-globalnav').width())/2,
-//            logoWidth = $('.nav-wrapper .portal-logo').width();
-//        if ($(window).width() > 767) {
-//            if ($('.top-image .portal-logo').css('display') == 'none' ) { 
-//                if (logoWidth > leftMargin ) {
-//                    $('#theme-globalnav').css({'margin-left': logoWidth });  
-//                } else {
-//                    $('#theme-globalnav').css({'margin' : '0 auto' });  
-//                }
-//            } else {
-//                $('#theme-globalnav').css({'margin-left': leftMargin});
-//            }
-//        }
-//    }
-//    function calculateMargin () {
-//        var leftMargin = (960 - $('#theme-globalnav').width())/2,
-//            logoWidth = $('.nav-wrapper .portal-logo').width();
-//        if ($(window).width() > 767) {
-//            $(window).bind('scroll', function() {   
-//                var topHeight = $(".top-block").height(),
-//                    logoWidth = $('.nav-wrapper .portal-logo').width();
-//
-//                if ($('.top-image .portal-logo').css('display') !== 'none' ) { 
-//                   if ($(window).scrollTop() > topHeight) {
-//                        if (logoWidth > leftMargin ) {
-//                            $('#theme-globalnav').css({'margin-left': logoWidth });  
-//                            }
-//                    } else {
-//                        $('#theme-globalnav').css({ 'margin-left': leftMargin });
-//                    }
-//                }
-//            });
-//        }    
-//    }
-	$('.nav-wrapper').resize(function() {
-		var wrapper = $('#theme-globalnav');
-		$('.nav-wrapper').css('line-height', '' + wrapper.height());
-	});	
+$(document).ready(function(){	
     $(window).resize(function() {
-        if ($(window).width() > 767) {
-            //onStart();
-
-//             calculateMargin();
-        } else {
-            $('#theme-globalnav').css({ 'margin-left': "auto" });
+        if ($(window).width() <= 767) {
+			$('#theme-globalnav').css({ 'margin-left': "auto" });
         }
     });
+	$(window).bind('scroll', function() {
+		$('.nav-wrapper').addClass('fixed');
+	});
 
-    $(window).bind('scroll', function() {
-        var topHeight = $(".top-block").height();
-        var el = $('.nav-placeholder');
-        if ($(window).width() > 0) {
-            if ($(window).scrollTop() > topHeight) {
-                if(!el.length) {
-                    $('.trigger-box').wrap('<div class="nav-placeholder"></div>');
-                    $('.nav-placeholder').height($('.trigger-box').outerHeight());
-                }
-                $('.trigger-box').addClass('fixed');
-                $('.trigger.fixed .portal-logo').css({'opacity':'1'}); 
-            } else {
-                el.before($('.trigger-box'));
-                el.remove();
-                $('.trigger-box').removeClass('fixed');
-                $('.trigger-box .portal-logo').css({'opacity':'0'});
-                }
-            }  
-    });
-    //onStart();
-//     calculateMargin();
-
-    $('label[for="nav-trigger"').bind('click', function() {
+    $('label[for="nav-trigger"]').bind('click', function() {
 		var wrapper = $('.nav-wrapper');
 		var themenav = $('#theme-globalnav');
+		var label = $('label[for="nav-trigger"]');
 		if (wrapper.attr('data-slid') === "slidout") {
-			themenav.animate({marginLeft: "-120em"}, 100).promise().done(function() {
-					themenav.css('display', 'none');
+			if($('.placeholder').length <= 0) {
+				wrapper.wrap("<div class='placeholder'></div>");
+				$('.placeholder').height(wrapper.height());
+			}
+			label.removeClass('left');
+			label.addClass('right');
+			wrapper.addClass('fixed');
+			themenav.animate({marginLeft: "-120em"}, 100); // TODO: Animate opacity to 0
+			wrapper.animate({width: "3em"}, 500);
+			$('.placeholder').animate({height: "0"}, 500).promise().done(function() {
+				wrapper.attr('data-slid', 'slidin');
 			});
-			wrapper.animate({width: "3em"}, 1000, function() {
-					wrapper.attr('data-slid', 'slidin');
+		} else if (wrapper.attr('data-slid') === "slidin") {
+			label.removeClass('right');
+			label.addClass('left');
+			wrapper.animate({width: "100%"}, 500); // TODO: Animate opacity to 100
+			themenav.animate({marginLeft: 0}, 100).promise().done(function() {
+				wrapper.attr('data-slid', 'slidout');
 			});
 			
-		} else if (wrapper.attr('data-slid') === "slidin") {
-			themenav.css('display', 'flex');
-			wrapper.animate({width: "100%"}, 500);
-			themenav.animate({marginLeft: 0}, 100, function() {
-					wrapper.attr('data-slid', 'slidout');
-				});
 		}
-        $('.nav_placeholder').height($('.nav-wrapper').height());
     });
 });
