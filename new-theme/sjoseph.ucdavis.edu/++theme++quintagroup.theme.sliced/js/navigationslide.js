675c5d6288e13c90aac63fa8940ead1f41879b3a
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2016-11-07 15:20:13 -0800

    reverting back to working navbar animation

--- new-theme/sjoseph.ucdavis.edu/++theme++quintagroup.theme.sliced/js/navigationslide.js
+++ new-theme/sjoseph.ucdavis.edu/++theme++quintagroup.theme.sliced/js/navigationslide.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(window).resize(function() {
         if ($(window).width() <= 767) {
-    		$('#theme-globalnav').css({ 'margin-left': "auto" });
+        	$('#theme-globalnav').css({ 'margin-left': "auto" });
         }
     });
 
@@ -30,7 +30,7 @@ $(document).ready(function(){
 			label.addClass('harryPotter');
 		}
 	});
-	
+
 	var animateOut = function(label, wrapper, themenav) {
 		wrapper.attr('data-status', 'slidout');
 		label.removeClass('fa-angle-double-right');
@@ -38,7 +38,7 @@ $(document).ready(function(){
 		wrapper.animate({width: "100%"}, 500); // TODO: Animate opacity to 100
 		themenav.animate({marginLeft: 0}, 100);
 	}
-	
+
 	var createPlaceholder = function(wrapper) {
 		wrapper.wrap("<div class='placeholder'></div>");
 		$('.placeholder').height(wrapper.height());
@@ -62,36 +62,8 @@ $(document).ready(function(){
 			animateIn(label, wrapper, themenav);
 		} else if (wrapper.attr('data-status') === "slidin") {
 			animateOut(label, wrapper, themenav);
-
-		if (wrapper.attr('data-slid') === "slidout") {
-			if($('.placeholder').length <= 0) {
-				wrapper.wrap("<div class='placeholder'></div>");
-				$('.placeholder').height(wrapper.height());
-			}
-			label.removeClass('left');
-			label.addClass('right');
-            label.removeClass('fa-angle-double-left')
-            label.addClass('fa-angle-double-right')
-			wrapper.addClass('fixed');
-			themenav.animate({marginLeft: "-120em"}, 100); // TODO: Animate opacity to 0
-			wrapper.animate({width: "5em"}, 500);
-			$('.placeholder').animate({height: "0"}, 500).promise().done(function() {
-				wrapper.attr('data-slid', 'slidin');
-			});
-		} else if (wrapper.attr('data-slid') === "slidin") { // TODO: If scrollTop is zero, then animate placeholder
-			label.removeClass('right');						 // 	  back out to full width.
-			label.addClass('left');
-            label.removeClass('fa-angle-double-right')
-            label.addClass('fa-angle-double-left')
-			if ($(window).scrollTop() == 0) {
-				$('.placeholder').animate({height: wrapper.height()}, 500);
-			}
-			wrapper.animate({width: "100%"}, 500); // TODO: Animate opacity to 100
-			themenav.animate({marginLeft: 0}, 100).promise().done(function() {
-				wrapper.attr('data-slid', 'slidout');
-			});
 		}
     });
-	
+
 	$('.nav-wrapper').css('min-height', "" + $('#theme-globalnav').height());
 });
